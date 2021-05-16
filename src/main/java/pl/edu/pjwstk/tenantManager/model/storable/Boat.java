96e877616c65dd571042fa35e6177a965723d31c
Paweł Dąbrowski <fluorix@gmail.com>
2021-05-16 22:00:57 +0200

    Make some classes for model.

---
+++ src/main/java/pl/edu/pjwstk/tenantManager/model/storable/Boat.java
@@ -0,0 +1,4 @@
+package pl.edu.pjwstk.tenantManager.model.storable;
+
+public class Boat implements Vehicle {
+}
---
+++ src/main/java/pl/edu/pjwstk/tenantManager/model/storable/Vehicle.java
@@ -0,0 +1,6 @@
+package pl.edu.pjwstk.tenantManager.model.storable;
+
+import pl.edu.pjwstk.tenantManager.model.storable.Storable;
+
+public interface Vehicle extends Storable {
+}
