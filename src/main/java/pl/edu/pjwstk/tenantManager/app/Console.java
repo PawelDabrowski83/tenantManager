e0dc500b21e7aef9f9686090627b89c48e0fd06d
Paweł Dąbrowski <fluorix@gmail.com>
2021-05-25 23:34:36 +0200

    Use text block instead of line concatenation.

--- src/main/java/pl/edu/pjwstk/tenantManager/app/Console.java
+++ src/main/java/pl/edu/pjwstk/tenantManager/app/Console.java
@@ -2,10 +2,11 @@ package pl.edu.pjwstk.tenantManager.app;
 
 public class Console {
     public static final String MSG_INTRO = "Witaj w aplikacji TENANT_MANAGER.";
-    public static final String MSG_MAIN_MENU = "Wybierz opcję:\n" +
-            "1. Zaloguj się.\n" +
-            "2. Obejrzyj zasoby developera.\n" +
-            "0. Wyjście z aplikacji.";
+    public static final String MSG_MAIN_MENU = """
+            Wybierz opcję:
+            1. Zaloguj się.
+            2. Obejrzyj zasoby developera.
+            0. Wyjście z aplikacji.""";
     public static final String MSG_EXIT = "Do zobaczenia!";
 
     public static void run(){
